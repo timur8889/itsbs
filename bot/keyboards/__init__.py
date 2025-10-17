6f31daf0dab1c660de2473cad24716ccd7a271c0
Timur <45330851+timur8889@users.noreply.github.com>
2025-10-17 20:08:42 +0300

    Update __init__.py

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,1 +1,8 @@
-
+from .inline import (
+    get_main_menu, 
+    get_categories_keyboard, 
+    get_priorities_keyboard,
+    get_admin_keyboard,
+    get_request_actions,
+    get_confirmation_keyboard
+)
